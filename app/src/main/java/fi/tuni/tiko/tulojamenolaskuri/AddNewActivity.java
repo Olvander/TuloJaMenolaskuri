895739bd9b50f31113e39f7af6fe46ef485564b9
Olli Pertovaara <olli.pertovaara@gmail.com>
2020-03-26 10:47:19 +0200

    Update several files to implement a date picker dialog.

--- app/src/main/java/fi/tuni/tiko/tulojamenolaskuri/AddNewActivity.java
+++ app/src/main/java/fi/tuni/tiko/tulojamenolaskuri/AddNewActivity.java
@@ -2,15 +2,57 @@ package fi.tuni.tiko.tulojamenolaskuri;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+/**
+ * This class is for showing add new income or expense event activity.
+ */
 public class AddNewActivity extends AppCompatActivity {
+
+    /**
+     * An instance of DateSelectorFragment class.
+     *
+     * Used for displaying a Date Picker Dialog.
+     */
+    private DateSelectorFragment dateSelector;
+
+    /**
+     * A TextView to hold the current or selected date.
+     */
+    private TextView dateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+
+        TextView dateText = findViewById(R.id.dateText);
+        this.dateText = dateText;
+        dateText.setText(getFormattedDate());
+    }
+
+    /**
+     * Return the formatted current date.
+     * @return The formatted date
+     */
+    public String getFormattedDate() {
+
+        Calendar c = GregorianCalendar.getInstance();
+        String weekDayText = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT,
+                Locale.getDefault());
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH) + 1;
+        int year = c.get(Calendar.YEAR);
+
+        String formattedDate = String.format("%s %s.%s.%s", weekDayText, day, month, year);
+
+        return formattedDate;
     }
 
     public void onCancelPressed(View v) {
@@ -19,5 +61,16 @@ public class AddNewActivity extends AppCompatActivity {
 
     public void onSavePressed(View v) {
 
+    }
+
+    /**
+     * Show DatePickerDialog when Choose date has been pressed
+     * @param v The view whose onclick method was called
+     */
+    public void onSetDatePressed(View v) {
+        if (dateSelector == null) {
+            dateSelector = new DateSelectorFragment(this, dateText);
+        }
+        dateSelector.showDialog();
     }
 }
--- app/src/main/java/fi/tuni/tiko/tulojamenolaskuri/DateSelectorFragment.java
+++ app/src/main/java/fi/tuni/tiko/tulojamenolaskuri/DateSelectorFragment.java
@@ -1,4 +1,99 @@
 package fi.tuni.tiko.tulojamenolaskuri;
 
-public class DateSelectorFragment {
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import java.util.Calendar;
+import java.util.Locale;
+
+/**
+ * This class is for showing a DAtePickerDialog fragment.
+ */
+public class DateSelectorFragment extends DialogFragment implements
+        DatePickerDialog.OnDateSetListener  {
+
+    /**
+     * The context that this instance was initialized from
+     */
+    private Context context;
+
+    /**
+     * A TextView that holds the selected or current date
+     */
+    private TextView dateText;
+
+    /**
+     * A DatePicker that holds the selected date
+     */
+    private DatePicker datePicker;
+
+    /**
+     * Constructor that initializes the context and a TextView
+     * @param context   The context that this constructor was called from
+     * @param dateText  A TextView that holds the date as text
+     */
+    public DateSelectorFragment(Context context, TextView dateText) {
+        this.context = context;
+        this.dateText = dateText;
+    }
+
+    /**
+     * This method can be called to show a date picker dialog.
+     */
+    public void showDialog() {
+        this.show(((AddNewActivity) context).getSupportFragmentManager(),
+                "Date Picker");
+    }
+
+    /**
+     * Returns a date picker dialog with either the current date
+     * or a previously selected date if it is created more than once.
+     * @param savedInstanceState    The saved instance state
+     * @return A new DatePickerDialog with current or previously selected date
+     */
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        if (datePicker != null) {
+            year = datePicker.getYear();
+            month = datePicker.getMonth();
+            day = datePicker.getDayOfMonth();
+        }
+        return new DatePickerDialog(getActivity(), R.style.CalendarTheme, this,
+                year, month, day);
+    }
+
+    /**
+     * Updates a textView from another activity with the selected date.
+     *
+     * @param view          A DatePicker
+     * @param year          The selected year
+     * @param month         The selected month
+     * @param dayOfMonth    The selected day
+     */
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        this.datePicker = view;
+        month += 1;
+        Calendar c = Calendar.getInstance();
+        c.set(year, month, dayOfMonth);
+
+        String weekDay = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT,
+                Locale.getDefault());
+        String formattedDate = String.format("%s %s.%s.%s", weekDay, dayOfMonth, month, year);
+        this.dateText.setText(formattedDate);
+    }
 }
